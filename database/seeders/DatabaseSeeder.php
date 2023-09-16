d8ecd37daffdc9de8627404fd631606a36235dcd
Thiago Barros <thiagobarros95@gmail.com>
2023-09-16 17:53:53 -0300

    feat: inserção de dados para popular database básico

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Event::insert([
+            [
+                'name' => 'Evento de programação backend java',
+                'start_date' => now()->addDays(10)->format('Y-m-d'),
+                'end_date' => now()->addDays(12)->format('Y-m-d'),
+                'status' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Evento de programação backend php',
+                'start_date' => now()->addDays(10)->format('Y-m-d'),
+                'end_date' => now()->addDays(12)->format('Y-m-d'),
+                'status' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Evento de programação frontend',
+                'start_date' => now()->addDays(10)->format('Y-m-d'),
+                'end_date' => now()->addDays(12)->format('Y-m-d'),
+                'status' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Evento de programação frontend em reactjs',
+                'start_date' => now()->addDays(10)->format('Y-m-d'),
+                'end_date' => now()->addDays(12)->format('Y-m-d'),
+                'status' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Evento de programação frontend em nextjs',
+                'start_date' => now()->addDays(10)->format('Y-m-d'),
+                'end_date' => now()->addDays(12)->format('Y-m-d'),
+                'status' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+        \App\Models\Event::factory()->count(5)->create();
     }
 }
