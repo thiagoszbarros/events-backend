c8341384370f42d42f15ec339d47396c8cc7250d
Thiago Barros <thiago.barros@rodobank.com.br>
2023-09-16 17:52:44 -0300

    feat: criacao de factory de eventos

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'start_date' => now()->addDays(10),
+            'end_date' => now()->addDays(12),
+            'status' => 1,
+        ];
+    }
+}
