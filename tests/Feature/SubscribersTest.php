8d8fc38936334ce293c2b9e19320ed8fe995fd58
Thiago Barros <thiago.barros@rodobank.com.br>
2023-09-16 19:03:47 -0300

    feat: testes automatizados para subscriber

---
+++ tests/Feature/SubscribersTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use Mockery;
+use Exception;
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Interfaces\SubscriberInterface;
+use App\Http\Requests\PaginationRequest;
+use App\Http\Requests\SubscriberRequest;
+use App\Http\Controllers\SubscriberController;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SubscribersTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_index(): void
+    {
+        $response = $this->get('/api/subscribers');
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        $this->assertSame(
+            'array',
+            gettype($response->original)
+        );
+
+        $this->assertSame(
+            'object',
+            gettype($response->original['data'])
+        );
+    }
+
+    public function test_store(): void
+    {
+        $response = $this->post('/api/subscribers', [
+            'name' => fake()->name,
+            'email' => fake()->safeEmail(),
+            'cpf' => fake()->numerify('###########'),
+        ]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+
+        $this->assertSame(
+            'array',
+            gettype($response->original)
+        );
+
+        $this->assertSame(
+            'string',
+            gettype($response->original['data'])
+        );
+
+        $this->assertSame(
+            'Subscrição realizada com sucesso.',
+            $response->original['data']
+        );
+    }
+
+    public function test_subscriber_controller_exceptions()
+    {
+        $subscriber = Mockery::mock(SubscriberInterface::class);
+        $log = new Log();
+        $subscriber->shouldReceive('index')
+            ->andThrow(new Exception())
+            ->shouldReceive('store')
+            ->andThrow(new Exception());
+
+        $resultIndex = (new SubscriberController($subscriber, $log))->index(new PaginationRequest);
+        $resultCreate = (new SubscriberController($subscriber, $log))->store(new SubscriberRequest);
+
+        $this->assertSame(
+            $resultIndex->original,
+            ['data' => []]
+        );
+
+        $this->assertSame(
+            $resultCreate->original,
+            ['data' => 'Não foi possível realizar a subscrição.']
+        );
+    }
+}
