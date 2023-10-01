20c4b99b8b8966a8e806f3c6e34a7d6ddac03f9b
Thiago Barros <thiagobarros95@gmail.com>
2023-09-30 21:27:39 -0300

    chore: melhora json da rota /

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,25 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the 'web' middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return [
+        'name' => 'thiagoszbarros/events-backend',
+        'type' => 'project',
+        'description' => '',
+        'authors' => [
+            [
+                'name' => 'Thiago Barros'
+            ]
+        ],
+        'keywords' => [
+            'laravel',
+            'framework'
+        ],
+        'license' => 'MIT',
+        'laravel' => app()->version(),
+    ];
 });
