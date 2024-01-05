3f60a27005ed31e06c8f08480ada49b8d9d3654f
Thiago Barros <thiagobarros95@gmail.com>
2024-01-05 15:01:49 -0300

    chore: configura tratamento de exceptions de forma global

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception): Response
+    {
+        return new Response(
+            [
+                'data' => 'Opa! Algo deu errado. Tente novamente mais tarde.'
+            ],
+            Response::HTTP_UNPROCESSABLE_ENTITY
+        );
     }
 }
